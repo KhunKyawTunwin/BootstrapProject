cbea1ea7ab8d4ec147bc43e21e7d308904de1cc8
KhunKyawTunwin <66564687+KhunKyawTunwin@users.noreply.github.com>
2023-02-01 23:58:35 +0630

    Frontend With bootstrapProject

---
+++ script.js
@@ -0,0 +1,37 @@
+// class Dog {
+//  constructor(name) {
+//   this.name = name;
+//  }
+//  run() {
+//   console.log(`${this.name} is running....`);
+//  }
+// }
+
+// let dog1 = new Dog("Burdy Dog");
+
+// console.log(dog1.run());
+
+// //
+
+// function outerFunction(outerVariable) {
+//  return function innerFunction(innerVariable) {
+//   console.log(`Outer Variable ${outerVariable}`);
+//   console.log(`Inner Variable ${innerVariable}`);
+//  };
+// }
+
+// const newFunction = outerFunction("outside"); //newFunction('inside')
+// newFunction("inside");
+
+// function aa(outerFunction) {
+//  cancelIdleCallback();
+// }
+
+// // $("#myBtn").click(function () {
+// //  alert("Welcome to My Our it school");
+// // });
+
+// function Clck() {
+//  alert("Welcome to my school");
+// }
+alert(window.location.href);
